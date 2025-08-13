203af0361b3ebc707db31369e0cdd55f4bf86fcc
Paulo Roberto <pr98.dev@gmail.com>
2025-08-13 10:41:28 -0300

    feat: created Employee class as entity and applying JPA annotations

--- EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Employee/EmployeeModel.java
+++ EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Employee/EmployeeModel.java
@@ -1,4 +1,60 @@
 package com.dev.EmployeeRegistration.Employee;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_register")
 public class EmployeeModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private Integer age;
+  private String email;
+  private String position;
+
+
+  public EmployeeModel() {
+  }
+
+  public EmployeeModel(String name, Integer age, String email, String position) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.position = position;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public void setPosition(String position) {
+    this.position = position;
+  }
 }
