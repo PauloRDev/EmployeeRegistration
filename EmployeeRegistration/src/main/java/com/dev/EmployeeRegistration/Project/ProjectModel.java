e48e1eb42e09aa45364085c69e6d5b5bf0fafc6c
Paulo Roberto <pr98.dev@gmail.com>
2025-08-14 11:07:18 -0300

    refactor : ProjectModel receive EmployeeModel reference for table relationship

--- EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectModel.java
+++ EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectModel.java
@@ -1,4 +1,54 @@
 package com.dev.EmployeeRegistration.Project;
 
+import com.dev.EmployeeRegistration.Employee.EmployeeModel;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_project")
 public class ProjectModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String projectName;
+  private String task;
+  private String projectLevel;
+  private EmployeeModel employee;
+
+
+  public ProjectModel() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getProjectName() {
+    return projectName;
+  }
+
+  public void setProjectName(String projectName) {
+    this.projectName = projectName;
+  }
+
+  public String getTask() {
+    return task;
+  }
+
+  public void setTask(String task) {
+    this.task = task;
+  }
+
+  public String getProjectLevel() {
+    return projectLevel;
+  }
+
+  public void setProjectLevel(String projectLevel) {
+    this.projectLevel = projectLevel;
+  }
 }
