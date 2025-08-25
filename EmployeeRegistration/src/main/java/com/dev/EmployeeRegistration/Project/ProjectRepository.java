98cd2d764126c79a5ef2104fdf2d0fabf1b48a25
Paulo Roberto <pr98.dev@gmail.com>
2025-08-25 09:28:04 -0300

    feat: created ProjectRepository interface and extends ProjectModel

---
+++ EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectRepository.java
@@ -0,0 +1,6 @@
+package com.dev.EmployeeRegistration.Project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<ProjectModel, Long> {
+}
