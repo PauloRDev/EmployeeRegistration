60b363be276817a8a2b0d8e7b33bb55ad1c1f9cf
Paulo Roberto <pr98.dev@gmail.com>
2025-09-23 09:29:37 -0300

    feat: created the base routes for ProjectController CRUD

--- EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectController.java
+++ EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectController.java
@@ -1,15 +1,33 @@
 package com.dev.EmployeeRegistration.Project;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/projects")
 public class ProjectController {
 
-  @GetMapping("/projects")
-  public String projects() {
-    return "My list of projects";
+  @PostMapping("/create")
+  public String createProject () {
+    return "Project created";
+  }
+
+  @GetMapping("/all")
+  public String showAllProjects () {
+    return "all projects";
+  }
+
+  @GetMapping("/projectID")
+  public String showAllProjectByID() {
+    return "Project by ID";
+  }
+
+  @PutMapping("/updateID")
+  public String updateProjectByID() {
+    return "updated project data by ID";
+  }
+
+  @DeleteMapping("/deleteID")
+  public String deleteProjectByID() {
+    return "Delete Project by ID";
   }
 }
