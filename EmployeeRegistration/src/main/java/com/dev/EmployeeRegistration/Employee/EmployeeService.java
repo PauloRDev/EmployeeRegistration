30f94ac78a6b73625fa2fb220aa276c832187ed5
Paulo Roberto <pr98.dev@gmail.com>
2025-09-24 10:07:07 -0300

    feat: created employeeService with List of EmployeeModel and findAll

--- EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Employee/EmployeeService.java
+++ EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Employee/EmployeeService.java
@@ -1,4 +1,20 @@
 package com.dev.EmployeeRegistration.Employee;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class EmployeeService {
+
+  private EmployeeRepository employeeRepository;
+
+  public EmployeeService(EmployeeRepository employeeRepository) {
+    this.employeeRepository = employeeRepository;
+  }
+
+  // list all employee
+  public List<EmployeeModel> listEmployee() {
+    return employeeRepository.findAll();
+  }
 }
