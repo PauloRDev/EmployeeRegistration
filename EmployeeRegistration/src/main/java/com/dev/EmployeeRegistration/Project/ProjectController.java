61442a475a30728b69950a33532c97427a7b3120
Paulo Roberto <pr98.dev@gmail.com>
2025-10-15 11:08:06 -0300

    fix: Project controller had a base data, now was change to logic

--- EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectController.java
+++ EmployeeRegistration/src/main/java/com/dev/EmployeeRegistration/Project/ProjectController.java
@@ -1,33 +1,70 @@
 package com.dev.EmployeeRegistration.Project;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/projects")
+@RequestMapping("/project")
 public class ProjectController {
 
+  private final ProjectService projectService;
+
+  public ProjectController(ProjectService projectService) {
+    this.projectService = projectService;
+  }
+
+  // CREATE
   @PostMapping("/create")
-  public String createProject () {
-    return "Project created";
+  public ResponseEntity<String> createProject(@RequestBody ProjectDTO project) {
+    ProjectDTO newProject = projectService.createProject(project);
+    return ResponseEntity.status(HttpStatus.CREATED)
+            .body("Project successfully created " + newProject.getProjectName() + " (ID) " + newProject.getId());
   }
 
-  @GetMapping("/all")
-  public String showAllProjects () {
-    return "all projects";
+  // GET
+  @GetMapping("/list")
+  public ResponseEntity<List<ProjectDTO>> listProject() {
+    List<ProjectDTO> project = projectService.listProject();
+    return ResponseEntity.ok(project);
   }
 
-  @GetMapping("/projectID")
-  public String showAllProjectByID() {
-    return "Project by ID";
+  @GetMapping("list/{id}")
+  public ResponseEntity<?> listAllProjectByID(@PathVariable Long id) {
+    ProjectDTO project = projectService.listAllProject(id);
+
+    if (project != null) {
+      return ResponseEntity.ok(project);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+              .body("this project by ID: " + id + " don't exist");
+    }
   }
 
-  @PutMapping("/updateID")
-  public String updateProjectByID() {
-    return "updated project data by ID";
+  // PUT
+  @PutMapping("/update/{id}")
+  public ResponseEntity<?> updateProject(@PathVariable Long id, @RequestBody ProjectDTO updateProject) {
+    ProjectDTO project = projectService.updateProjectByID(id, updateProject);
+
+    if (project != null) {
+      return ResponseEntity.ok(project);
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+              .body("Project by this ID: " + id + " not exist");
+    }
   }
 
-  @DeleteMapping("/deleteID")
-  public String deleteProjectByID() {
-    return "Delete Project by ID";
+  // DELETE
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<String> deleteProject(@PathVariable Long id) {
+    if (projectService.listAllProject(id) != null) {
+      projectService.deleteProjectByID(id);
+      return ResponseEntity.ok("Project by ID " + id + " Successfully deleted");
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+              .body("The project with ID " + id + " not found");
+    }
   }
 }
